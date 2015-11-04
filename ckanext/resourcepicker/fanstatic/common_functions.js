549e21a2ae83f6d35d57c34000cc8aeab4d93f8c
root <root@rsg-ckan.npm.ac.uk>
2015-11-04 11:39:40 +0000

    Corrected the url_get function

--- ckanext/resourcepicker/fanstatic/common_functions.js
+++ ckanext/resourcepicker/fanstatic/common_functions.js
@@ -1,6 +1,5 @@
 var url_get=function url_get(url, sParam) { // A function for getting parameters from the url
-   url = url.replace("http://", ""); // Takes the 'http://' from the start of the url (added by CKAN)
-   url = url.replace(/&amp;/g, "&"); // Takes all '&amp;' from the the url and makes it a normal '&'
+   url.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&").replace("http://", "")
    var sPageURL = url,
       sURLVariables = sPageURL.split('&'),
       sParameterName,
